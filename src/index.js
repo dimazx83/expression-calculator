7b50ab16ee4c812f610b0aa2d53e3a4581a8f636
Марина Пискуненко <C:\Users\Asus\AppData\Roaming\The Bat!\Mail>
2019-10-12 20:41:33 +0300

    init

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    function mult(m){
+        return m.split('*').map(i=>+i).reduce((acc,i)=>acc*i)
+      }
+      function division(div){
+        return div.split('/').map(i=>mult(i)).reduce((acc,i)=>acc/i)
+      }
+      function minus(min){
+        return min.split('-').map(i=>division(i)).reduce((acc,i)=>acc-i)
+      }
+      function sum(s){
+       return s.split('+').map(i=>minus(i)).reduce((acc,i)=>acc+i);
+      }
+      return sum(expr)
 }
 
 module.exports = {
